41f03459a3007c61b139cedba6954948416a80cb
Ricardo <44562760+RicardoDeAngelis@users.noreply.github.com>
2019-07-06 22:38:49 -0300

    alerta

--- js/script.js
+++ js/script.js
@@ -119,7 +119,7 @@ class Juego {
     }else{
       // perdio el juego
       this.perdioElJuego()
-            swal('llegaste al nivel:',this.nivel)
+            swal("nivel",this.nivel)
 
     }
   }
